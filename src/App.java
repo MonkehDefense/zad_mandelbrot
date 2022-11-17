0ceda5dc07b3fd2ab9a14d7e16e5600f8232d548
Kuba Pezda <kuba.pezda@interia.pl>
2022-11-17 14:51:05 +0100

    dawaj, save_it i time_it

--- src/App.java
+++ src/App.java
@@ -1,31 +1,45 @@
 import java.awt.image.BufferedImage;
+import java.awt.Color;
 import java.io.File;
 import java.lang.Math;
+import java.lang.*;
 
 public class App {
 //    private int h, w;
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+//        gen_pic(300, 300);
+
+        int[] widths = new int[]{32,64,128,256,512,1024,2048,4096,8192};
+        int k = 20;
+        int[] repeats = new int[]{k,k,k,k,k,k,k,k,k};
+
+        time_it(widths, widths, repeats);
+
         //Czas ten należy wyznaczyć dla obrazków o bokach 32, 64, 128, 256, 512, 1024, 2048, 4096 i 8192 piksele.
         //Należy też narysować wykres
-        //domyślnie max_iter = 200
-        //domyślnie zakres
-        //ci: [-1.2, 1.2]
-        //cr: [-2.1, 0.6]
+    }
+
+    
+    public static BufferedImage gen_pic(int w, int h, double cr_left, double cr_right, double ci_top, double ci_bottom){
+        return gen_pic(w,h,cr_left,cr_right,ci_top,ci_bottom, 200);
+    }
+    
+    public static BufferedImage gen_pic(int w, int h, int iter){
+        return gen_pic(w,h, -2.1,.6, 1.2, -1.2, iter);
+    }
+    
+    public static BufferedImage gen_pic(int w, int h){
+        return gen_pic(w,h, -2.1,.6, 1.2, -1.2, 200);
     }
 
     public static BufferedImage gen_pic(int w, int h, double cr_left, double cr_right, double ci_top, double ci_bottom, int iter) {
         double zr, zi, z_abs, cr, ci, cr_span, ci_span;
+        int clr_aux;
         cr_span = cr_right - cr_left;
         ci_span = ci_top - ci_bottom;
 
-        //int[] palette = new int[iter];
-        //for(int i = 0; i < iter; i++){
-        //    palette[i] = color.getColor();
-        //}
-
-        BufferedImage img = new BufferedImage(w,h,1);
+        BufferedImage img = new BufferedImage(w,h,BufferedImage.TYPE_INT_RGB);
 
         for(int y = 0; y < h; y++){
             for(int x = 0; x < w; x++){
@@ -50,9 +64,10 @@ public class App {
                 }
 
                 if(itr == iter){
-//                   img.setRGB(x, y, color[end]);
+                   img.setRGB(x, y, new Color(100,0,0).getRGB());
                 } else {
-//                    img.setRGB(x, y, color[itr]);
+                    clr_aux = 255 - (int)Math.floor(255.0 * (double)itr/(double)iter);
+                    img.setRGB(x, y, new Color(clr_aux,255,clr_aux).getRGB());
                 }
 
 
@@ -61,7 +76,46 @@ public class App {
         return img;
         }
 
-    public static double time_it(int w, int h, int repeat) {
-        return 1.0;
+    
+
+    public static void time_it(int[] w, int[] h, int[] repeat) {
+
+        if(w.length == h.length || repeat.length == w.length){
+            long[] times = new long[repeat.length];
+            long stop;
+            BufferedImage[] images = new BufferedImage[repeat.length];
+
+            for(int i = 0; i < repeat.length; i++){
+
+                times[i] = System.nanoTime();
+                for(int j = 0; j < repeat[i]; j++){
+                    images[i] = gen_pic(w[i], h[i]);
+                }
+                stop = System.nanoTime();
+
+                times[i] = (stop - times[i])/repeat[i];
+            }
+
+            //zapisać czasy i obrazki
+            save_it(images, times);
+
+        }
     }
+
+    private static void save_it(BufferedImage img, String filename){
+        System.out.println("");
+    }
+
+    private static void save_it(BufferedImage[] images, long[] times){
+        int w, h;
+        for(int i = 0; i < images.length; i++){
+            w = images[i].getWidth();
+            h = images[i].getHeight();
+            //zapisać obraz w na h pikseli
+            save_it(images[i], "name");
+            //zrobić logi czas-wymiar
+            
+        }
+    }
+
 }
